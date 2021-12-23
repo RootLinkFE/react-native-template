a0272dd5730f260ce3addc99aacb53a7796605c9
giscafer <giscafer@outlook.com>
2021-12-23 21:17:37 +0800

    chore: rename

--- android/app/src/main/java/com/leekhub/roothubapp/MainApplication.java
+++ android/app/src/main/java/com/leekhub/roothubapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.rnzustandboilerplate;
+package com.leekhub.roothubapp;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 import com.facebook.react.bridge.JSIModulePackage;
 import android.app.Application;
@@ -68,7 +68,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.rnzustandboilerplate.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.leekhub.roothubapp.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
--- ios/RootHubAppTests/RootHubAppTests.m
+++ ios/RootHubAppTests/RootHubAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RNZustandBoilerPlateTests : XCTestCase
+@interface RootHubAppTests : XCTestCase
 
 @end
 
-@implementation RNZustandBoilerPlateTests
+@implementation RootHubAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
