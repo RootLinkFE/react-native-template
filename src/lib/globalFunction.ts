ab6aabd7445a934fcdd465c2ca49f42aecf07ecc
giscafer <giscafer@outlook.com>
2021-12-26 17:16:18 +0800

    chore: $toast

--- src/lib/globalFunction.ts
+++ src/lib/globalFunction.ts
@@ -2,13 +2,29 @@
  * @author giscafer
  * @email giscafer@outlook.com
  * @create date 2021-12-24 17:52:47
- * @modify date 2021-12-24 17:52:47
+ * @modify date 2021-12-26 17:13:59
  * @desc 全局单例方法
  */
 
 import { Toast } from 'native-base';
 
-(global as any).$toast = Toast.show({
-  title: 'Hello world',
-  placement: 'top',
-});
+/**
+ * 全局单例toast
+ */
+(global as any).$toast = (
+  content: string,
+  duration: number = 2000,
+  placement:
+    | 'top'
+    | 'top-right'
+    | 'top-left'
+    | 'bottom'
+    | 'bottom-left'
+    | 'bottom-right' = 'top',
+) => {
+  Toast.show({
+    title: content,
+    placement,
+    duration,
+  });
+};
