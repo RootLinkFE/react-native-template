e40e855c4f878ccfc9db85ac50e8dd117a227411
zhongmingxing <mingxing.zhong@irootcloud.com>
2022-01-06 17:36:13 +0800

    fix: remove async-storage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import create, { GetState, SetState, StoreApi } from 'zustand';
 import { persist } from 'zustand/middleware';
 import { createLoginSlice, ILoginSlice } from './createLoginSlice';
